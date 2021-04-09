afa81c72a14c6bd2df837cd4d759105adf3a12d4
jjlovaglio <jjlovaglio@gmail.com>
2021-04-09 16:50:32 -0300

    chore: auth0 setup

--- setup.sh
+++
@@ -1,8 +0,0 @@
-export DATABASE_URL="postgresql://localhost/fsndcapstoneherokutest"
-export FLASK_APP=run
-export FLASK_ENV=development
-echo "Database URL set to:"$DATABASE_URL
-echo "Flask app set to: "$FLASK_APP
-echo "Environment mode set to: "$FLASK_ENV
-
-# run with $ source setup.sh
