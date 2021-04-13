400bcfef2c358011b02df64f3bf06b73e669efae
jjlovaglio <jjlovaglio@gmail.com>
2021-04-13 16:00:27 -0300

    bugfix: need to fix missing authorization header bug

--- main/auth.py
+++ main/auth.py
@@ -4,10 +4,8 @@ import json
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-from main import app
 
-# app instance running in __init__.py
-# app = Flask(__name__)
+
 
 AUTH0_DOMAIN = 'jjlovaglio.us.auth0.com'
 # AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
@@ -121,11 +119,4 @@ def requires_auth(f):
     return wrapper
 
 
-@app.route('/headers')
-@requires_auth
-def headers(payload):
-    print(payload)
-    return 'Access Granted'
 
-if __name__ == '__main__':
-    app.run()
--- main/run.py
+++ main/run.py
@@ -31,7 +31,6 @@ from flask import url_for
 from authlib.integrations.flask_client import OAuth
 from six.moves.urllib.parse import urlencode
 
-
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -157,6 +156,92 @@ class AuthError(Exception):
         self.status_code = status_code
 
 
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
+
+
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
                         raise AuthError({
@@ -171,7 +256,6 @@ def check_permissions(permission, payload):
         }, 403)
     return True
 
-
 def requires_auth(permission=''):
   def requires_auth_decorator(f):
     @wraps(f)
@@ -179,7 +263,12 @@ def requires_auth(permission=''):
       if 'profile' not in session:
         # Redirect to Login page here
         return redirect('/login')
-
+      jwt = get_token_auth_header()
+      try:
+        payload = verify_decode_jwt(jwt)
+      except:
+        abort(401)
+        
       check_permissions(permission, payload)
 
       return f(*args, **kwargs)
